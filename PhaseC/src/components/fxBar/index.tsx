edbd73fb0c0945598b413167e527a27b1ac14309
John Henry Rudden <johnhenryrudden@johns-mbp.myfiosgateway.com>
2021-11-29 15:56:19 -0500

    add functionality to update store when grid cell content changes

--- PhaseC/src/components/fxBar/index.tsx
+++ PhaseC/src/components/fxBar/index.tsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "./FXBar.css";
 import { SelectedCell } from "../../types";
+import { useSelector } from "react-redux";
+import { ApplicationState } from "../../store";
 
 interface Props {
     selectedCell: SelectedCell | null;
 }
 
 const FXBar: React.FC<Props> = ({ selectedCell }) => {
+    const [fxValue, setFXValue] = useState("");
+    const grid = useSelector((state: ApplicationState) => state.grid.grid);
+
+    useEffect(() => {
+        if (selectedCell) {
+            const cellContents: string =
+                grid[selectedCell!.row][selectedCell!.column].content;
+            setFXValue(cellContents);
+        } else {
+            setFXValue("");
+        }
+    }, [selectedCell]);
     return (
         <div className="fxBar">
             <div className="selectCell">
                 <div className="cellName">
                     {selectedCell ? selectedCell.cellLabel.toUpperCase() : ""}
                 </div>
-                <ArrowDropDownIcon />
             </div>
             <div className="fx">fx</div>
-            <input className="function" />
+            <input
+                className="function"
+                value={fxValue}
+                readOnly
+                disabled={true}
+            />
         </div>
     );
 };
--- PhaseC/src/components/grid/gridCell/index.tsx
+++ PhaseC/src/components/grid/gridCell/index.tsx
@@ -1,4 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { handleBreakpoints } from "@mui/system";
+import React, { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ApplicationState } from "../../../store";
+import { replaceContent } from "../../../store/grid/actions";
 import { Cell } from "../../../store/grid/types";
 import { SelectedCell } from "../../../types";
 import "./GridCell.css";
@@ -12,9 +16,28 @@ interface Props {
 
 const GridCell: React.FC<Props> = ({ cell, setSelectedCell, col, row }) => {
     const [cellContent, setCellContent] = useState(cell.content);
+    const dispatch = useDispatch();
+    const ref = useRef<HTMLInputElement>(null);
+    const state = useSelector((state: ApplicationState) => state.grid);
     const repeatCharacterCount = Math.ceil((col + 1) / 26);
     const character: string = String.fromCharCode(97 + (col % 26));
-    const cellName = `${character.repeat(repeatCharacterCount)}${row}`;
+    const cellName = `${character.repeat(repeatCharacterCount)}${row + 1}`;
+
+    const submitContent = () => {
+        if (cellContent !== cell.content) {
+            dispatch(replaceContent(state, cellContent, row, col));
+        }
+    };
+
+    const handleKeys = (e: React.KeyboardEvent) => {
+        if (e.key === "Enter") {
+            console.log("We are here");
+            submitContent();
+            if (ref.current) {
+                ref.current.blur();
+            }
+        }
+    };
 
     useEffect(() => {
         setCellContent(cell.content);
@@ -23,6 +46,7 @@ const GridCell: React.FC<Props> = ({ cell, setSelectedCell, col, row }) => {
     return (
         <div className="cell">
             <input
+                ref={ref}
                 value={cellContent}
                 onChange={(e) => setCellContent(e.target.value)}
                 onSelect={() =>
@@ -32,6 +56,8 @@ const GridCell: React.FC<Props> = ({ cell, setSelectedCell, col, row }) => {
                         cellLabel: cellName,
                     })
                 }
+                onKeyPress={handleKeys}
+                onBlur={submitContent}
             />
         </div>
     );
