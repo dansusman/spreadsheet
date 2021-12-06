d3cedf2745737fa418385555fd988c534c3c84f6
tangemily <tang.s.emily@gmail.com>
2021-12-06 15:45:19 -0500

    parsing tests

--- PhaseC/src/util/util.spec.ts
+++ PhaseC/src/util/util.spec.ts
@@ -22,14 +22,22 @@ describe("Parsing Tests", () => {
         return grid;
     }
 
-    let ROWS = 2;
-    let COLS = 2;
+    let ROWS = 5;
+    let COLS = 5;
 
     const initialGrid = makeGrid(ROWS, COLS);
     var modifiedGrid: Cell[][]
 
     beforeEach(() => {
         modifiedGrid = JSON.parse(JSON.stringify(initialGrid));
+    });
+
+    it("attempt arithmetic without equals sign", () => {
+        modifiedGrid[0][0].content = "2 + 3";
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("2 + 3");
     });
 
     it("basic arithmetic", () => {
@@ -40,14 +48,88 @@ describe("Parsing Tests", () => {
         expect(parsedCell.evaluate().content).to.deep.equal("5");
     });
 
+    it("arithmetic with multiple operations and parens", () => {
+        modifiedGrid[0][0].content = "= ((2 + 2) * 9 / 6 - 2) * 10 / 2.5";
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("16");
+    });
+
+    it("attempt SUM without equals sign", () => {
+        modifiedGrid[0][0].content = "SUM(A1..A2)";
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("SUM(A1..A2)");
+    });
+
     it("SUM over two cells", () => {
         modifiedGrid[0][0].content = "= SUM(B1..B2)";
-        modifiedGrid[0][1].content = "= 1";
-        modifiedGrid[1][1].content = "= 1";
+        modifiedGrid[0][1].content = "= 1"; // B1
+        modifiedGrid[1][1].content = "= 1"; // B2
 
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
 
         expect(parsedCell.evaluate().content).to.deep.equal("2");
+    });
+
+    it("SUM over many cells", () => {
+        modifiedGrid[0][0].content = "= SUM(B1..D5)";
+        modifiedGrid[0][1].content = "= 10"; // B1
+        modifiedGrid[1][1].content = "= 143"; // B2
+        modifiedGrid[4][1].content = "= 71"; // B5
+        modifiedGrid[0][2].content = "= 4"; // C1
+        modifiedGrid[2][2].content = "= -23"; // C3
+        modifiedGrid[0][3].content = "= 7.2"; // D1
+        modifiedGrid[4][3].content = "= -5"; // D5
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("207.2");
+    });
+
+    it("SUM of REF and AVG", () => {
+        modifiedGrid[0][0].content = "= SUM(B1..D5)";
+        modifiedGrid[1][0].content = "= 2"; // A2
+        modifiedGrid[2][0].content = "= 20"; // A3
+        modifiedGrid[3][0].content = "= 8"; // A4
+
+        modifiedGrid[0][1].content = "= REF(A2)"; // B1
+        modifiedGrid[1][1].content = "= 143"; // B2
+        modifiedGrid[4][1].content = "= 71"; // B5
+        modifiedGrid[0][2].content = "= 4"; // C1
+        modifiedGrid[2][2].content = "= -15"; // C3
+        modifiedGrid[0][3].content = "= AVG(A2..A4)"; // D1
+        modifiedGrid[4][3].content = "= -5"; // D5
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("210");
+    });
+
+    it("SUM updates when REF updates", () => {
+        modifiedGrid[0][0].content = "= SUM(B1..B2)";
+        modifiedGrid[1][0].content = "= 2"; // A2
+        modifiedGrid[2][0].content = "= 20"; // A3
+
+        modifiedGrid[0][1].content = "= REF(A2)"; // B1
+        modifiedGrid[1][1].content = "= REF(A3)"; // B2
+
+        modifiedGrid[1][0].content = "= 50"; // A2
+        modifiedGrid[2][0].content = "= 20"; // A3
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("70");
+    });
+
+    it("attempt AVG without equals sign", () => {
+        modifiedGrid[0][0].content = "AVG(A2..A3)";
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("AVG(A2..A3)");
     });
 
     it("AVG over two cells", () => {
@@ -58,6 +140,64 @@ describe("Parsing Tests", () => {
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
 
         expect(parsedCell.evaluate().content).to.deep.equal("5");
+    });
+
+    it("AVG over many cells", () => {
+        modifiedGrid[0][0].content = "= AVG(B1..D5)";
+        modifiedGrid[0][1].content = "= 10"; // B1
+        modifiedGrid[1][1].content = "= 143"; // B2
+        modifiedGrid[4][1].content = "= 71"; // B5
+        modifiedGrid[0][2].content = "= 4"; // C1
+        modifiedGrid[2][2].content = "= -23"; // C3
+        modifiedGrid[0][3].content = "= 40"; // D1
+        modifiedGrid[4][3].content = "= -5"; // D5
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("16");
+    });
+
+    it("AVG updates when REF updates", () => {
+        modifiedGrid[0][0].content = "= AVG(B1..B2)"; // A1
+        modifiedGrid[1][0].content = "= 2"; // A2
+        modifiedGrid[2][0].content = "= 20"; // A3
+
+        modifiedGrid[0][1].content = "= REF(A2)"; // B1
+        modifiedGrid[1][1].content = "= REF(A3)"; // B2
+
+        modifiedGrid[1][0].content = "= 50"; // A2
+        modifiedGrid[2][0].content = "= 20"; // A3
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("35");
+    });
+
+    it("AVG of REF and SUM", () => {
+        modifiedGrid[0][0].content = "= AVG(B1..D5)";
+        modifiedGrid[1][0].content = "= 2"; // A2
+        modifiedGrid[2][0].content = "= 20"; // A3
+        modifiedGrid[3][0].content = "= 8"; // A4
+
+        modifiedGrid[0][1].content = "= REF(A2)"; // B1
+        modifiedGrid[1][1].content = "= 138"; // B2
+        modifiedGrid[4][1].content = "= 71"; // B5
+        modifiedGrid[0][2].content = "= 4"; // C1
+        modifiedGrid[2][2].content = "= -15"; // C3
+        modifiedGrid[0][3].content = "= SUM(A2..A4)"; // D1
+        modifiedGrid[4][3].content = "= -5"; // D5
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("15");
+    });
+
+    it("attempt REF without equals sign", () => {
+        modifiedGrid[0][0].content = "REF(A2)";
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("REF(A2)");
     });
 
     it("basic REF", () => {
@@ -77,12 +217,105 @@ describe("Parsing Tests", () => {
         expect(parsedCell.evaluate().content).to.deep.equal("0");
     });
 
-    it("string concat", () => {
+    it("deep REFs", () => {
+        modifiedGrid[0][0].content = "= REF(A2)"; // A1
+        modifiedGrid[1][0].content = "= REF(B1)"; // A2
+        modifiedGrid[0][1].content = "= REF(B2)"; // B1
+        modifiedGrid[1][1].content = "= 5"; // B2
+
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 });
+
+        expect(parsedCell.evaluate().content).to.deep.equal("5");
+    });
+
+    it("reference an AVG cell", () => {
+        modifiedGrid[0][0].content = "= REF(A2)"; // A1
+        modifiedGrid[1][0].content = "= AVG(B1..B2)"; // A2
+        modifiedGrid[0][1].content = "= 10"; // B1
+        modifiedGrid[1][1].content = "= 2"; // B2
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("6");
+    });
+
+    it("reference a SUM cell", () => {
+        modifiedGrid[0][0].content = "= REF(A2)"; // A1
+        modifiedGrid[1][0].content = "= SUM(B1..B2)"; // A2
+        modifiedGrid[0][1].content = "= 10"; // B1
+        modifiedGrid[1][1].content = "= 2"; // B2
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("12");
+    });
+
+    it("cell updates when reference cell is deleted", () => {
+        modifiedGrid[1][0].content = "= 5"; // A2
+        modifiedGrid[0][0].content = "= REF(A2)"; // A1
+        modifiedGrid[1][0].content = ""; // A2
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("0");
+    });
+
+    it("cell updates when reference cell is modified", () => {
+        modifiedGrid[1][0].content = "= 5"; // A2
+        modifiedGrid[0][0].content = "= REF(A2)"; // A1
+        modifiedGrid[1][0].content = "\"zip\""; // A2
+
+        let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
+
+        expect(parsedCell.evaluate().content).to.deep.equal("zip");
+    });
+
+    it("basic string concat", () => {
         modifiedGrid[0][0].content = "\"zip\" + \"zap\"";
 
-        let parsedCell = new StringParser("\"zip\" + \"zap\"") // call initialGrid[0]?
+        let parsedCell = new StringParser(modifiedGrid[0][0].content);
 
         expect(parsedCell.evaluate()).to.deep.equal("zipzap");
     });
 
+    it("multiple string concats", () => {
+        modifiedGrid[0][0].content = "\"zip\" + \"zap\" + \"zip\" + \"zap\"";
+
+        let parsedCell = new StringParser(modifiedGrid[0][0].content);
+
+        expect(parsedCell.evaluate()).to.deep.equal("zipzapzipzap");
+    });
+
+    it("string concat with string number", () => {
+        modifiedGrid[0][0].content = "\"zip\" + \"4\" + \"zap\"";
+
+        let parsedCell = new StringParser(modifiedGrid[0][0].content);
+
+        expect(parsedCell.evaluate()).to.deep.equal("zip4zap");
+    });
+
+    it("attempt string concat with number", () => {
+        modifiedGrid[0][0].content = "\"zip\" + 4 + \"zap\"";
+
+        let parsedCell = new StringParser(modifiedGrid[0][0].content);
+
+        expect(parsedCell.evaluate()).to.deep.equal("\"zip\" + 4 + \"zap\"");
+    });
+
+    it("attempt string concat with non-string", () => {
+        modifiedGrid[0][0].content = "\"zip\" + zap";
+
+        let parsedCell = new StringParser(modifiedGrid[0][0].content);
+
+        expect(parsedCell.evaluate()).to.deep.equal("\"zip\" + zap");
+    });
+
+    it("attempt string concat with open quotes", () => {
+        modifiedGrid[0][0].content = "\"zip + \"zap\"";
+
+        let parsedCell = new StringParser(modifiedGrid[0][0].content);
+
+        expect(parsedCell.evaluate()).to.deep.equal("\"zip + \"zap\"");
+    });
 });
