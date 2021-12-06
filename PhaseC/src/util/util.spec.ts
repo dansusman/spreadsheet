3c6467fec7a6ccb1bae86c8d7bd27dcd98d90ba0
tangemily <tang.s.emily@gmail.com>
2021-12-06 12:17:22 -0500

    added beforeAll modifiedGrid

--- PhaseC/src/util/util.spec.ts
+++ PhaseC/src/util/util.spec.ts
@@ -25,25 +25,22 @@ describe("Parsing Tests", () => {
     let ROWS = 2;
     let COLS = 2;
 
-    let initialGrid = makeGrid(ROWS, COLS);
+    const initialGrid = makeGrid(ROWS, COLS);
+    var modifiedGrid: Cell[][]
 
-    let gridState: GridState = { grid: initialGrid, rows: ROWS, columns: COLS, redoStack: [], undoStack: [] };
-
+    beforeEach(() => {
+        modifiedGrid = JSON.parse(JSON.stringify(initialGrid));
+    });
 
     it("basic arithmetic", () => {
-        let modifiedGrid: Cell[][] = initialGrid;
         modifiedGrid[0][0].content = "= 2 + 3";
 
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), {y: 0, x: 0})
 
         expect(parsedCell.evaluate().content).to.deep.equal("5");
-
-        gridState = { grid: initialGrid, rows: ROWS, columns: COLS, redoStack: [], undoStack: [] };
     });
 
     it("SUM over two cells", () => {
-        let modifiedGrid: Cell[][] = initialGrid;
-
         modifiedGrid[0][0].content = "= SUM(B1..B2)";
         modifiedGrid[0][1].content = "= 1";
         modifiedGrid[1][1].content = "= 1";
@@ -51,13 +48,9 @@ describe("Parsing Tests", () => {
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
 
         expect(parsedCell.evaluate().content).to.deep.equal("2");
-
-        gridState = { grid: initialGrid, rows: ROWS, columns: COLS, redoStack: [], undoStack: [] };
     });
 
     it("AVG over two cells", () => {
-        let modifiedGrid: Cell[][] = initialGrid;
-
         modifiedGrid[0][0].content = "= AVG(B1..B2)";
         modifiedGrid[0][1].content = "= 2";
         modifiedGrid[1][1].content = "= 8";
@@ -65,45 +58,31 @@ describe("Parsing Tests", () => {
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
 
         expect(parsedCell.evaluate().content).to.deep.equal("5");
-
-        gridState = { grid: initialGrid, rows: ROWS, columns: COLS, redoStack: [], undoStack: [] };
     });
 
     it("basic REF", () => {
-        let modifiedGrid: Cell[][] = initialGrid;
-
         modifiedGrid[0][0].content = "= REF(A2)"; // A1
         modifiedGrid[1][0].content = "= 8"; // A2
 
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 })
 
         expect(parsedCell.evaluate().content).to.deep.equal("8");
-
-        gridState = { grid: initialGrid, rows: ROWS, columns: COLS, redoStack: [], undoStack: [] };
     });
 
     it("empty REF", () => {
-        let modifiedGrid: Cell[][] = initialGrid;
-
         modifiedGrid[0][0].content = "= REF(A2)";
-
-        initialGrid[1][0].content = "";
 
         let parsedCell = new FunctionParser(modifiedGrid, modifiedGrid[0][0].content.trim(), { y: 0, x: 0 });
 
         expect(parsedCell.evaluate().content).to.deep.equal("0");
-
-        gridState = { grid: initialGrid, rows: ROWS, columns: COLS, redoStack: [], undoStack: [] }; // TODO: do we still need to set this?
     });
 
     it("string concat", () => {
-        initialGrid[0][0].content = "\"zip\" + \"zap\"";
+        modifiedGrid[0][0].content = "\"zip\" + \"zap\"";
 
         let parsedCell = new StringParser("\"zip\" + \"zap\"") // call initialGrid[0]?
 
         expect(parsedCell.evaluate()).to.deep.equal("zipzap");
-
-        gridState = initialState;
     });
 
 });
